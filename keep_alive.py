a602c3c375065a85507b20e4e6d1287ef5ad9042
PhiLong305 <philinhvip00@gmail.com>
2025-08-31 13:39:57 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -9,7 +9,8 @@ def home():
     return "Bot is alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+    port=int(os.environ.get("PORT", 0))
+    app.run(host='0.0.0.0', port=port, use_reloader=False)
 
 def keep_alive():
     t = Thread(target=run, daemon=True)
